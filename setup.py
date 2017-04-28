d5376a656b0917c38c65da34a0cee2868c068095
TimWhiting <tim@whitings.org>
2017-04-28 16:42:31 -0600

    another commit

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 import subprocess
 import sys
 import shutil
-import ecen_330_overlay
+import ecen_330
 
 setup(
     name = "ecen_330",
-    version = ecen_330.__1__,
+    version = ecen_330.__version__,
     url = 'https://github.com/TimWhiting/ecen_330_pynq',
     license = 'All rights reserved.',
     author = "Tim Whiting",
     author_email = "tim@whitings.com",
     packages = ['ecen_330'],
     package_data = {
-    '' : ['*.bit','*.tcl','*.py','*.so'],
+    '' : ['*.bit','*.tcl','*.py','*.so','*.elf'],
     },
     description = "New custom overlay for PYNQ-Z1"
 )
