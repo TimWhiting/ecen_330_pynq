f224065fe09bee303a885db0df3e5194e0669068
TimWhiting <tim@whitings.org>
2017-04-29 11:25:39 -0600

    stuff

--- ecen_330/ecen_330.py
+++ ecen_330/ecen_330.py
@@ -9,27 +9,27 @@ ECEN330_EXE_DIR = os.path.join(ECEN330_ROOT_DIR, 'executables')
 ECEN330_BIT_DIR = os.path.join(ECEN330_ROOT_DIR, 'bitstream')
 
 class ecen330:
-    
-    def __init__(self):
-        self.bitstream_name="top.bit"
-        self.bitstream_path=os.path.join(ECEN330_BIT_DIR, self.bitstream_name)
-        if PL.bitfile_name != self.bitstream_path:
-            if load_overlay:
-                Overlay(self.bitstream_path).download()
-            else:
-                raise RuntimeError("Incorrect Overlay loaded")     
-        
-    def getExecutables(self):
-        names = ""
-        for root, dirs, files in os.walk(ECEN330_EXE_DIR):
-            for name in files:
-                names += name + " "
-        return names
-        
-    def execute(self,name):
-        fullName = ECEN330_EXE_DIR + name
-        process = subprocess.Popen([fullName],stdout=PIPE)
-        if process.stdout:
-            print process.stdout.readlines()
-    
+	
+	def __init__(self):
+		self.bitstream_name="top.bit"
+		self.bitstream_path=os.path.join(ECEN330_BIT_DIR, self.bitstream_name)
+		if PL.bitfile_name != self.bitstream_path:
+			if load_overlay:
+				Overlay(self.bitstream_path).download()
+			else:
+				raise RuntimeError("Incorrect Overlay loaded")     
+		
+	def getExecutables(self):
+		names = ""
+		for root, dirs, files in os.walk(ECEN330_EXE_DIR):
+			for name in files:
+				names += name + " "
+		return names
+		
+	def execute(self,name):
+		fullName = ECEN330_EXE_DIR + name
+		process = subprocess.Popen([fullName],stdout=PIPE)
+		if process.stdout:
+			for line in proc.stdout:
+				print line
 
