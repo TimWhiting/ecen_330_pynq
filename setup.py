c35c84b6823d0ac443aa296d027782e57ca0a306
TimWhiting <tim@whitings.org>
2017-04-29 11:56:04 -0600

    stuff

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ import ecen_330
 import os
 
 setup(
-    name = "ecen_330",
+    name = "ecen_330_pynq",
     version = ecen_330.__version__,
     url = 'https://github.com/TimWhiting/ecen_330_pynq',
     license = 'All rights reserved.',
     author = "Tim Whiting",
     author_email = "tim@whitings.org",
-	include_package_data = True,
+    include_package_data = True,
     packages = ['ecen_330'],
     package_data = {
-    '' : ['*.bit','*.tcl','*.so','*.bin','*.txt', '*.cpp', '*.h', '*.sh'],
-    }, 
-	data_files = [(os.path.join('/home/xilinx/jupyter_notebooks/ecen_330',root.replace('notebooks/','')), [os.path.join(root, f) for f in files]) for root, dirs, files in os.walk('notebook/')],
+    'ecen_330' : ['*.bit','*.tcl','*.so','*.bin','*.txt', '*.cpp', '*.h', '*.sh'],
+    },
+    data_files = [(os.path.join('/home/xilinx/jupyter_notebooks/ecen_330',root.replace('notebook/','')), [os.path.join(root, f) for f in files]) for root, dirs, files in os.walk('notebook/')],
     description = "New custom overlay for PYNQ-Z1"
 )
